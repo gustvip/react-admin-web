256371adcf1f3b98c7ac27f6337f35b89e9c9861
joey <zhouhaifei1994@sina.com>
2018-06-07 23:09:33 +0800

    更新package

--- build/webpack.config.prod.js
+++ build/webpack.config.prod.js
@@ -36,7 +36,29 @@ module.exports = merge(baseConfig, {
 			new UglifyJsPlugin({
 				cache: true,
 				parallel: true,
-				sourceMap: true, // set to true if you want JS source maps
+				sourceMap: false,
+				uglifyOptions: {
+					output: {
+						// 最紧凑的输出
+						beautify: false,
+						// 删除所有的注释
+						comments: false,
+					},
+					compress: {
+						// 在UglifyJs删除没有用到的代码时不输出警告
+						warnings: false,
+						
+						drop_console: false,
+						
+						// 内嵌定义了但是只用到一次的变量
+						collapse_vars: true,
+						
+						// 提取出出现多次但是没有定义成变量去引用的静态值
+						reduce_vars: true,
+						
+						comparisons: false,
+					},
+				},
 			}),
 			new OptimizeCssAssetsPlugin(),
 		],
