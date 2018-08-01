dba22a2d970fe325ceb58f427413559dfc63c9b0
joey <zhouhaifei1994@sina.com>
2018-08-01 18:40:16 +0800

    update package

--- src/routes/test/components/demo/index.js
+++ src/routes/test/components/demo/index.js
@@ -1,39 +1,21 @@
-//import CesiumComponent from 'templates/tool_components/cesium'
+import CesiumComponent from 'templates/tool_components/cesium'
 import T from 'utils/t'
+// import * as  THREE from 'three'
 
 export default class Component extends React.PureComponent {
-	constructor () {
-		super()
-		this.viewer = null
-	}
-	
-	componentDidMount () {
-		console.log(this.powerSet([1, 2, 3]))
-	}
-	
-	powerSet (originalSet) {
-		const subSets = []
-		
-		const numberOfCombinations = 2 ** originalSet.length
-		for (let combinationIndex = 0; combinationIndex < numberOfCombinations; combinationIndex += 1) {
-			const subSet = []
-			
-			for (let setElementIndex = 0; setElementIndex < originalSet.length; setElementIndex += 1) {
-				// Decide whether we need to include current element into the subset or not.
-				if (combinationIndex & (1 << setElementIndex)) {
-					subSet.push(originalSet[setElementIndex])
-				}
-			}
-			
-			subSets.push(subSet)
-		}
-		
-		return subSets
-	}
-	
-	render () {
-		return (
-			<div>hello</div>
-		)
-	}
+  constructor () {
+    super()
+    this.container = null
+  }
+  
+  render () {
+    return (
+      <div
+        style={{position: 'absolute', left: 0, right: 0, top: 0, bottom: 0}}
+        ref={container => this.container = container}
+      >
+        <CesiumComponent/>
+      </div>
+    )
+  }
 }
