0ab986916769516362dc2c39c7d47ab508df318b
joey <zhouhaifei1994@sina.com>
2018-09-12 11:57:56 +0800

    add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,26 @@ const eslintrc = {
 		browser: true,
 		node: true,
 		jest: true,
-		es6: true,
+	},
+	globals: {
+		React: true,
+		ReactDOM: true,
+		mountNode: true,
+		ENV: false,
 	},
 	parser: 'babel-eslint',
 	plugins: [
 		'react',
+		'babel',
 	],
 	rules: {
+		'import/extensions': 0,
+		'import/no-unresolved': 0,
+		'import/no-extraneous-dependencies': 0,
+		'import/newline-after-import': 0,
+		
+		'no-shadow': 0,
+		'spaced-comment': 0,
 		'func-names': 0,
 		'arrow-body-style': 0,
 		'react/sort-comp': 0,
@@ -18,10 +31,7 @@ const eslintrc = {
 		'react/jsx-first-prop-new-line': 0,
 		'react/jsx-one-expression-per-line': 0,
 		'react/forbid-prop-types': 0,
-		'react/jsx-filename-extension': [1, {extensions: ['.js', '.jsx', '.md']}],
-		'import/extensions': 0,
-		'import/no-unresolved': 0,
-		'import/no-extraneous-dependencies': 0,
+		'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.md'] }],
 		'prefer-destructuring': 0,
 		'no-param-reassign': 0,
 		'no-return-assign': 0,
@@ -39,68 +49,26 @@ const eslintrc = {
 		'object-curly-newline': 0,
 		'no-restricted-globals': 0,
 		
-		// 标准缩进,2个空格
-		'indent': [
-			'error',
-			2,
-			{
-				'SwitchCase': 1,
-			},
-		],
-		// 换行符,unix换行符
-		'linebreak-style': [
-			'error',
-			'unix',
-		],
-		// 字符串引号,使用单引号
-		'quotes': [
-			'error',
-			'single',
-		],
-		// 分号,必须
-		'semi': [
-			'error',
-			'always',
-		],
-		// http://eslint.org/docs/rules/max-len
-		'padded-blocks': 0,
-		// (关闭)i++,++1
+		'no-console': 0,
 		'no-plusplus': 0,
-		// (关闭)
-		'default-case': 0,
-		// jsdoc注释
-		'require-jsdoc': [
-			'error',
-			{
-				'require': {
-					// 函数描述
-					'FunctionDeclaration': true,
-					// 方法定义
-					'MethodDefinition': false,
-					// 类描述
-					'ClassDeclaration': false,
-					// 闭包函数描述
-					'ArrowFunctionExpression': true,
-				},
-			},
-		],
-		// jsx的缩进
-		'react/jsx-indent': [
-			'error',
-			2,
-		],
-		// JSX 属性缩进
-		// https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-indent-props.md
-		'react/jsx-indent-props': [
-			'error',
-			2,
-		],
-		// (关闭)JSX组件在无使用声明周期方法,setState和ref时,必须声明成一个方法
-		// https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/prefer-stateless-function.md
+		'eol-last': 0,
+		'no-script-url': 0,
+		'prefer-rest-params': 0,
+		'react/no-access-state-in-setstate': 0,
+		'react/destructuring-assignment': 0,
+		'react/no-multi-comp': 0,
 		'react/prefer-stateless-function': 0,
-		'jsx-a11y/label-has-for': 0,
 		'jsx-a11y/href-no-hash': 0,
 		
+		// 标准缩进,2个空格
+		'indent': [1, 2, { 'SwitchCase': 1 }],
+		// 分号,必须
+		'semi': [2, 'always'],
+		// jsx的缩进
+		'react/jsx-indent': [1, 2],
+		// JSX 属性缩进
+		// https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-indent-props.md
+		'react/jsx-indent-props': [1, 2],
 	},
 };
 
--- src/index.js
+++ src/index.js
@@ -10,40 +10,20 @@
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import * as React from 'react';
-
-/**
- * 加载基础样式
- */
 import './base.scss';
-
-/**
- * 加载 redux store
- */
+import ErrorBoundary from 'templates/toolComponents/errorBoundary';
 import store from './store';
-
-/**
- * 加载路由
- */
 import Routes from './routes/index';
 
-/**
- * 错误处理程序
- */
-import ErrorBoundary from 'templates/toolComponents/errorBoundary';
-
-/**
- * 渲染程序
- */
-const renderApp = Routes => {
-	const wrapper = document.createElement('div');
-	wrapper.id = 'wrapper';
-	return render(
-		<ErrorBoundary>
-			<Provider store={store()}>
-				<Routes/>
-			</Provider>
-		</ErrorBoundary>
-		,
-		document.body.appendChild(wrapper));
+const renderApp = (Routes) => {
+  const wrapper = document.createElement('div');
+  wrapper.id = 'wrapper';
+  return render(
+    <ErrorBoundary>
+      <Provider store={store()}>
+        <Routes />
+      </Provider>
+    </ErrorBoundary>,
+    document.body.appendChild(wrapper));
 };
 renderApp(Routes);
