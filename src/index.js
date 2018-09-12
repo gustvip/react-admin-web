19e807ee1e4a8f348024eed933e5bca435751811
joey <zhouhaifei1994@sina.com>
2018-09-12 22:24:31 +0800

    add lint-stage

--- src/index.js
+++ src/index.js
@@ -7,23 +7,24 @@
  */
 // import './mock';
 
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import * as React from 'react';
-import './base.scss';
-import ErrorBoundary from 'templates/toolComponents/errorBoundary';
-import store from './store';
-import Routes from './routes/index';
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import * as React from "react";
+import "./base.scss";
+import ErrorBoundary from "templates/toolComponents/errorBoundary";
+import store from "./store";
+import Routes from "./routes/index";
 
-const renderApp = (Routes) => {
-	const wrapper = document.createElement('div');
-	wrapper.id = 'wrapper';
+const renderApp = Routes => {
+	const wrapper = document.createElement("div");
+	wrapper.id = "wrapper";
 	return render(
 		<ErrorBoundary>
 			<Provider store={store()}>
 				<Routes />
 			</Provider>
 		</ErrorBoundary>,
-		document.body.appendChild(wrapper));
+		document.body.appendChild(wrapper)
+	);
 };
 renderApp(Routes);
